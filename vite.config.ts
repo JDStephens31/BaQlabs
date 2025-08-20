bfa272c5c281465a907ddd073e8d71b304482437
Jonathan Stephens <83253662+JDStephens31@users.noreply.github.com>
2025-08-20 12:20:57 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
         ]
       : []),
   ],
-  base: "/",
+  base: "/BaQlabs",
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
